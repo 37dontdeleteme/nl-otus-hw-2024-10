db5f73be969dc5abdf3c32f9df8d0c256f5efb5e
nl <milkshameuw@yandex.ru>
2024-11-12 00:29:36 +0500

    fsaf

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/love/otus/bad/nl-otus-hw-2024-10
+# Build directory: /home/love/otus/bad/nl-otus-hw-2024-10/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/love/otus/bad/nl-otus-hw-2024-10/CMakeLists.txt;85;add_test;/home/love/otus/bad/nl-otus-hw-2024-10/CMakeLists.txt;0;")
